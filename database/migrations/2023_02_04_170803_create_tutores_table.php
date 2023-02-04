010bf7b9840b827f7764244af4571b7779e210bd
Gilberto <gilberto.vi.ro@gmail.com>
2023-02-04 12:24:15 -0600

    creacion de migracion para la BD data-excel

---
+++ database/migrations/2023_02_04_170803_create_tutores_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutores', function (Blueprint $table) {
+            $table->id('id_tutor');
+            $table->string('nombre_completo', 200);
+            $table->string('curp', 22);
+            $table->string('sexo', 20);
+            $table->string('n_telefono', 14)->nullable();
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')
+                    ->references('id_usuario')
+                    ->on('usuarios')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutores');
+    }
+}
---
+++ database/migrations/2023_02_04_170954_create_info_escolar_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoEscolarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_escolar', function (Blueprint $table) {
+            $table->id('id_info_escolar');
+            $table->string('nivel_estudio', 50);
+            $table->double('ultima_calificacion')->length(2);
+            $table->string('tipo_ciclo', 50);
+            $table->tinyInteger('grado')->length(2);
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')
+                    ->references('id_usuario')
+                    ->on('usuarios')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info_escolar');
+    }
+}
